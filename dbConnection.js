e8f564b3217ca6a252e8d464b23848162b15149d
Amrita Singh <82503836+Amrita1007@users.noreply.github.com>
2025-11-03 20:18:41 +0000

    Add files via upload

---
+++ dbConnection.js
@@ -0,0 +1,29 @@
+
+const mysql = require('mysql2/promise');
+
+let pool;
+const getConnection = async () => {
+  if (!pool) {
+    const config = {
+      host: 'metro.proxy.rlwy.net',
+      port: 36227,
+      user: 'root',
+      password: 'lAngSqcgBcnpRgtAZsCjkXJNdhuEXDag',
+      database: 'railway',
+      connectTimeout: 10000,
+    };
+
+    console.log('DB CONFIG (public proxy):', {
+      host: config.host,
+      port: config.port,
+      user: config.user,
+      database: config.database,
+    });
+
+    pool = mysql.createPool({ ...config, connectionLimit: 5 });
+    console.log('MySQL pool created');
+  }
+  return pool;
+};
+
+module.exports = { getConnection };
